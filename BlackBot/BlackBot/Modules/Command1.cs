fa8fc0dfbd5a9236d34c69ef2d11edc9878f4a4f
lkk6057 <30610351+lkk6057@users.noreply.github.com>
2018-05-16 16:28:38 -0400

    added base

---
+++ BlackBot/BlackBot/Modules/Command1.cs
@@ -0,0 +1,32 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlackBot.Modules
+{
+    public class Command1 : ModuleBase<SocketCommandContext>
+    {
+        [Command("hi")]
+        public async Task hiAsync()
+        {
+            await ReplyAsync("Hello!");
+        }
+        [Command("bye")]
+        public async Task byeAsync()
+        {
+            await ReplyAsync("Goodbye!");
+        }
+        [Command("count")]
+        public async Task countAsync()
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                await ReplyAsync(i.ToString());
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
