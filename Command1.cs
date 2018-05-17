568b8443cc47d393ce3c16b39133c906b4f1a638
lkk6057 <30610351+lkk6057@users.noreply.github.com>
2018-05-16 21:50:33 -0400

    d

---
+++ Command1.cs
@@ -0,0 +1,66 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using Discord;
+
+namespace BlackBot.Modules
+{
+    public class Command1 : ModuleBase<SocketCommandContext>
+    {
+        
+        [Command("hi")]
+        public async Task hiAsync()
+        {
+            await ReplyAsync("Hello!");
+        }
+        [Command("bye")]
+        public async Task byeAsync()
+        {
+            await ReplyAsync("Goodbye!");
+        }
+        [Command("count")]
+        public async Task countAsync()
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                await ReplyAsync(i.ToString());
+                Thread.Sleep(200);
+            }
+        }
+        [Command("echo")]
+        public async Task echoAsync([Remainder]string echo)
+        {
+            if (Context.User.Username == "Lkklkklkk"||Context.User.Username == "caulin_26546")
+            {
+                await ReplyAsync(echo);
+            }
+            else
+            {
+                await ReplyAsync($"No echo permission, {Context.User.Username}.");
+            }
+        }
+        [Command("agree")]
+        public async Task echoAsync()
+        {
+            if (Context.User.Username=="Lkklkklkk")
+            {
+                await ReplyAsync($"I agree with {Context.User.Username}.");
+            }
+            else
+            {
+                await ReplyAsync($"Fuck off {Context.User.Username}.");
+            }
+        }
+        [Command("roll")]
+        public async Task rollAsync(int range)
+        {
+            Random random = new Random();
+            await ReplyAsync($"{Context.User.Username} rolled a {random.Next(0,range)} out of {range}");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace BlackBot
+{
+    public class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+  
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+            string botToken = "NDQ2Mzg1NTQxMDg5MzI5MTUz.Dd4WWg.U8Ab7uy-m0HC_aYfiMHHXFQL6SA";
+
+            _client.Log += Log;
+            _client.UserJoined += AnnounceUserJoined;
+            await RegisterCommandsAsync();
+            await _client.LoginAsync(Discord.TokenType.Bot, botToken);
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private async Task AnnounceUserJoined(SocketGuildUser user)
+        {
+            var guild = user.Guild;
+            var channel = guild.DefaultChannel;
+            await channel.SendMessageAsync($"Welcome to the discord, {user.Mention}");
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            if (message is null || message.Author.IsBot)
+            {
+                return;
+            }
+            int argPos = 0;
+            if(message.HasStringPrefix("b!", ref argPos)||message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                var context = new SocketCommandContext(_client, message);
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
+
+        }
+    }
+}
